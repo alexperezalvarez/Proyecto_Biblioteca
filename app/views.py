40b3a9ed0d194011bda0ef551db69c3c5045d236
Alexander <Alexanderper974@gmail.com>
2025-03-20 11:35:04 -0500

    vistas basadas

--- app/views.py
+++ app/views.py
@@ -16,11 +16,11 @@ def crearAutor(request):
   else:
     autor_form = AutorForm()
     print(autor_form)
-  return render(request, 'crear_autor.html',{'autor_form': autor_form})
+  return render(request, 'libro/crear_autor.html',{'autor_form': autor_form})
 
 def listarAutor(request):
-  autores = Autor.objects.all()
-  return render(request, 'listar_autor.html', {'autores': autores})
+  autores = Autor.objects.filter(estado = True)
+  return render(request, 'libro/listar_autor.html', {'autores': autores})
 
 def editarAutor(request, id):
     try:
@@ -36,8 +36,16 @@ def editarAutor(request, id):
                 return redirect('index')  # Redirigir solo si se guarda el formulario
 
         # Si hay errores o es un método GET, renderiza el formulario
-        return render(request, 'crear_autor.html', {'autor_form': autor_form, 'error': error})
+        return render(request, 'lirbo/crear_autor.html', {'autor_form': autor_form, 'error': error})
 
     except ObjectDoesNotExist as e:
         error = f"Error: {e}"
-        return render(request, 'crear_autor.html', {'autor_form': None, 'error': error})
+        return render(request, 'libro/crear_autor.html', {'autor_form': None, 'error': error})
+
+def eliminarAutor(request, id):
+   autor = Autor.objects.get(id = id)
+   if request.method == 'POST':
+      autor.estado=False
+      autor.save()
+      return redirect('libro/listar_autor') 
+   return render(request, 'libro/eliminar_autor.html',{'autor':autor})
--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('crear_autor/', crearAutor, name='crear_autor'),
     path('listar_autor/', listarAutor, name='listar_autor'),
     path('editar_autor/<int:id>', editarAutor, name='editar_autor'),
+    path('eliminar_autor/<int:id>', eliminarAutor, name='eliminar_autor')
 ]
